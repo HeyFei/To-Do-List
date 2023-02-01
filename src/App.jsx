68d0a0a61c74d88f98498bba4b28ccd4d69c5a7a
HeyFei <shajiaming@gmail.com>
2023-02-01 17:57:56 +0800

    static page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+/*function App() {
+    return (
+        <div>Hello React World</div>
+    )
+}*/
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+const App = () => {
+    return (
+        <div className="App">
+            <div className='container'>
+                <h1>To-Do-List</h1>
+
+                <form className='toDoForm'>
+                    {/* 新增待辦事項的輸入框 */}
+                    <input type="text"/>
+                    {/* 新增按鈕 */}
+                    <button>Add</button>
+                </form>
+                {/* 待辦清單顯示內容 */}
+                <ul className='allToDoList'>
+                    <li className='singleTodoTask'>
+                        <span className='toDoText'>do the dishes</span>
+                        <button>Update</button>
+                        <button>Delete</button>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    )
 }
 
 export default App
